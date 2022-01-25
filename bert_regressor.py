ac9f86c5f600557ae462e731786489b0ae08d3ae
Filip Ginter <filip.ginter@gmail.com>
2022-01-25 21:01:37 +0200

    remove crud

--- bert_regressor.py
+++ bert_regressor.py
@@ -16,17 +16,10 @@ class BertRegressor(transformers.BertPreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
         self.bert = transformers.BertModel(config)
-        #The output layer that takes the [CLS] representation and gives an output
-        self.cls_layer1 = nn.Linear(config.hidden_size,128)
-        self.relu1 = nn.ReLU()
-        self.ff1 = nn.Linear(128,128)
-        self.tanh1 = nn.Tanh()
-        self.ff2 = nn.Linear(128,1)
+        #The output layer that takes the mean-pooled representation and gives an output
         self.ff3=nn.Linear(config.hidden_size,1)
 
     def forward(self, input_ids, attention_mask,target=None):
-        #print("INPUT IDS",input_ids.shape)
-        #print("Target",target)
         #Feed the input to Bert model to obtain contextualized representations
         outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
         #Obtain the representations of [CLS] heads
@@ -36,14 +29,10 @@ class BertRegressor(transformers.BertPreTrainedModel):
         h=h.sum(-2) #sum up along the token dimension
         h=h/attention_mask.sum(-1).unsqueeze(-1) #and divide by attmask sums to get average
         logits = h
-        #output = self.cls_layer1(logits)
-        #output = self.relu1(output)
-        #output = self.ff1(output)
-        #output = self.tanh1(output)
         output = self.ff3(logits)
-        if target is not None:
+        if target is not None: #Have a target? Calculate loss too!
             mo=RegressorModelOutput(output, nn.MSELoss(reduction="mean")(torch.squeeze(target,-1), torch.squeeze(output,-1)))
         else:
-            mo=RegressorModelOutput(output)
+            mo=RegressorModelOutput(output) #No target, get just the output
         return mo
         
